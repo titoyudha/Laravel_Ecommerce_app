3a18b0b9e18689921ee7a6d66ab99503d4f86236
titoyudha <tito.yudha.bimashika@gmail.com>
2023-06-04 02:14:24 +0700

    add product repositories

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\ProductContract;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use InvalidArgumentException;
+
+class ProductRepository extends BaseRepository implements ProductContract
+{
+    public function __construct(Product $model)
+    {
+        parent::__construct($model);
+        $this->model = $model;
+    }
+
+    public function listProduct(string  $order = "id", string $sort  = "desc", array $columns = ['*'])
+    {
+        return $this->all($columns, $order, $sort);
+    }
+
+    public function findProductById(int $id)
+    {
+        try {
+            return $this->findOneOrFail($id);
+        } catch(ModelNotFoundException $e) {
+
+            throw new ModelNotFoundException($e);
+        }
+    }
+
+    public function createProduct(array $params)
+    {
+        try {
+            $collection = collect($params);
+            $featured = $collection->has('featured') ? 1 : 0;
+            $status = $collection->has('status') ? 1 : 0;
+
+            $merge = $collection->merge(compact('status','featured'));
+
+            $product = new Product($merge->all());
+
+            $product->save();
+
+            if($collection->has('categories')){
+                $product->categories()->sync($params['categories']);
+            }
+            return $product;
+
+        } catch(QueryException $exception){
+            throw new InvalidArgumentException($exception->getMessage());
+        }
+    }
+
+    public function updateProduct(array $params)
+    {
+        $product = $this->findProductById($params['product_id']);
+
+        $collection = collect($params)->except('_token');
+
+        $featured = $collection->has('featured') ? 1 : 0;
+        $status = $collection->has('status') ? 1 : 0;
+
+        $merge = $collection->merge(compact('status', 'featured'));
+
+        $product->update($merge->all());
+
+        if ($collection->has('categories')) {
+            $product->categories()->sync($params['categories']);
+        }
+
+        return $product;
+    }
+
+    public function deleteProduct($id)
+    {
+        $product = $this->findProductById($id);
+
+        $product->delete();
+
+        return $product;
+    }
+}
